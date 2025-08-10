1f8098cd148bb56325c1f75fd862d34264493582
vibbyfs <vibbyfs@gmail.com>
2025-08-10 11:04:23 +0700

    feat: permintaan pertemanan, update pertemanan, hapus pertemenanan

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(cors())
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/users', authentication, require('./routes/users.routes'));
 app.use('/api/reminders', authentication, require('./routes/reminders.routes'));
+app.use('/api/friends', authentication, require('./routes/friends.routes'));
 
 
 app.use(erroHandler)
