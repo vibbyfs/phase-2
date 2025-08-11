71d81e0a83720da77d3d52ee5faa12102fe1d654
vibbyfs <vibbyfs@gmail.com>
2025-08-11 22:04:22 +0700

    chore: perbaiki endpoint whapify

--- services/waOutbound.js
+++ services/waOutbound.js
@@ -8,7 +8,7 @@ const axios = require('axios');
  */
 async function sendReminder(to, text, reminderId) {
   try {
-    const url = `${process.env.WHAPIFY_BASE_URL}/send-message`;
+    const url = `${process.env.WHAPIFY_BASE_URL}/wa_send`;
     const apiKey = process.env.WHAPIFY_API_KEY;
 
     if (!apiKey) {
